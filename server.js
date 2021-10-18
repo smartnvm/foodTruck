68c9aee90ad3123c9fc5441e12b5722084763e4c
AJ <aj@smartnvm.com>
2021-10-18 03:00:27 +0000

    - add admin login functionality

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 // load .env data into process.env
 require("dotenv").config();
 
+const { varInit, authenticateUser, getUserByEmail } = require('./lib/utils');
+
+// Requiring a JSON file automatically parses it and returns the data. These
+// are just example tweets to make it less tedious to style the app initially.
+const usersdB = require('./lib/admin');
+
+
+
 // Web server config
 const PORT = process.env.PORT || 8080;
 const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+
+const session = require("cookie-session");
+app.use(session({
+  name: 'session', keys: ['test'],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+
+
+
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -37,6 +54,7 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const users = require("./routes/users");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -49,14 +67,86 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
-  res.render("index");
+   //initialize template variable,
+  //if we are here we are not logged in
+  const templateVars = varInit(false, null, null, null);
+  res.render("index",templateVars);
 });
 
 
-app.get("/landing", (req, res) => {
-  res.render("index");
+
+
+app.get("/admin", (req, res) => {
+  //check if we are already logged in
+  const userId = req.session.user_id;
+  if (userId && usersdB[userId]) {
+    res.redirect('/orders');
+    return;
+  }
+  //initialize template variable,
+  //if we are here we are not logged in
+  const templateVars = varInit(false, null, null, null);
+  res.render('admin', templateVars);
 });
 
+app.post("/logout", (req, res) => {
+  //clears cookie and redirect to login page
+  req.session = null;
+  const templateVars = varInit(false, 200, null, null);
+  res.render('admin', templateVars);
+  return;
+});
+
+
+
+
+app.get("/orders", (req, res) => {
+  const userId = req.session.user_id;
+  const user = usersdB[userId];
+  console.log('-----users---------')
+
+  //check if user is logged in, and redirect to login if not
+  if (!user) {
+    const templateVars = varInit(false, 403, null, null)
+    res.render("admin", templateVars);
+    return;
+  }
+
+  //initalize template variable before passing to ejs view
+  const templateVars = varInit(true, 200, user, null);
+  res.render("menu_index", templateVars);
+});
+
+
+app.post("/admin", (req, res) => {
+  //parse user email and password
+  const email = req.body.username;
+  const password = req.body.password;
+
+  const user = getUserByEmail(email, usersdB);
+  console.log('user:', user);
+
+  //authenticate if matching user found
+  const authStatus = authenticateUser(email, password, user);
+
+  //authentication success - redirect to orders
+  if (user && authStatus.num === 200) {
+    req.session.user_id = user.id;
+    const templateVars = varInit(true, authStatus.num, user, null);
+    res.render('menu_index', templateVars);
+    return;
+  }
+
+  //authentication failed -
+  //redirect to login with appropriate error message
+  const templateVars = varInit(false, authStatus.num, user, null);
+  res.render('admin', templateVars);
+  return;
+
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+
+
