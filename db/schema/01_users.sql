a249351808b171285bc337dee88c33f36e94e12e
mays4 <maysalani84@yahoo.com>
2021-10-19 02:04:41 -0400

    adding tables

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,48 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS items CASCADE;
+DROP TABLE IF EXISTS items_orders CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  phone VARCHAR(32)  NOT NULL,
+  email VARCHAR(255)
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  cal INTEGER NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  image_url VARCHAR(255) NOT NULL,
+  price INTEGER  NOT NULL DEFAULT 0
+
+);
+CREATE TABLE items_orders(
+  id SERIAL PRIMARY KEY NOT NULL,
+  item_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  order_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  quantity INTEGER  NOT NULL DEFAULT 0
+
+);
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+  time  time(7)  NOT NULL,
+  estimated_time  time NOT NULL,
+  completed_time   time NOT NULL,
+  complete BOOLEAN NOT NULL DEFAULT TRUE
+
+
 );
